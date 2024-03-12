a82ee195fd8f438eccf793d7b7ea2faa73d3ae22
Saadak Ahmed <saadak98@hotmail.com>
2024-03-12 18:18:51 +0200

    Muokkaus

--- registeration.php
+++ registeration.php
@@ -1,13 +1,16 @@
 <?php
 include 'config/db_config.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+ 
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
+   
     $conn = new mysqli($servername, $username, $password, $dbname);
 
    
@@ -15,14 +18,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    
+  
     $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
     $stmt->bind_param("ss", $username, $hashed_password);
 
-   
+ 
     if ($stmt->execute()) {
-        echo "User registered successfully.";
-      
+        header("Location: login.html");
+        exit();
     } else {
         echo "Error registering user: " . $stmt->error;
     }
