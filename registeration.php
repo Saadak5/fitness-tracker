67ddbd14d8079fc11e517b7b20d290fe5de6bc71
Saadak Ahmed <saadak98@hotmail.com>
2024-03-13 01:22:47 +0200

    rekisteröityminen muokattu

--- registeration.php
+++ registeration.php
@@ -1,28 +1,22 @@
 <?php
 include 'config/db_config.php';
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
     $username = $_POST['username'];
     $password = $_POST['password'];
 
- 
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-   
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-   
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-  
     $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
     $stmt->bind_param("ss", $username, $hashed_password);
 
- 
     if ($stmt->execute()) {
         header("Location: login.html");
         exit();
