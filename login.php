39d5c8a718a7b352b58513895a7f5c0c20024d1e
Saadak Ahmed <saadak98@hotmail.com>
2024-03-13 01:16:55 +0200

    user id lisätty kirjautumiseen

--- login.php
+++ login.php
@@ -14,17 +14,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $stmt = $conn->prepare("SELECT password FROM users WHERE username = ?");
+    $stmt = $conn->prepare("SELECT user_id, password FROM users WHERE username = ?");
     $stmt->bind_param("s", $username);
 
     $stmt->execute();
     $stmt->store_result();
 
     if ($stmt->num_rows == 1) {
-        $stmt->bind_result($hashed_password);
+        $stmt->bind_result($user_id, $hashed_password);
         $stmt->fetch();
 
         if (password_verify($password, $hashed_password)) {
+            session_start();
+            $_SESSION['user_id'] = $user_id;
             header("Location: vote.html");
             exit();
         } else {
