34ef524885598f2d7010dd997b98abe759e042c6
Saadak Ahmed <saadak98@hotmail.com>
2024-03-12 18:10:42 +0200

    Käyttäjän kirjautuminen

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+include 'config/db_config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ 
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $stmt = $conn->prepare("SELECT password FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
+
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows == 1) {
+        $stmt->bind_result($hashed_password);
+        $stmt->fetch();
+
+        if (password_verify($password, $hashed_password)) {
+            echo "Login successful.";
+        } else {
+            echo "Incorrect password.";
+        }
+    } else {
+        echo "User not found.";
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
